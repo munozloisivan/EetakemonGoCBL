98467d1e85b95eaa9674f4d68fdf797c606b0f7c
Dani <danimb93@gmail.com>
2017-06-06 16:22:54 +0200

    RecuperarDatos funcional, falta configurar bien los alerts

--- public/web/scripts/recuperarDatosScript.js
+++ public/web/scripts/recuperarDatosScript.js
@@ -9,43 +9,45 @@ $(document).ready(function() {
         evento.preventDefault();
 
         var email = $('#correo').val();
-        var url = 'http://localhost:8080/myapp/datos';
+        var url = 'http://localhost:8080/myapp/usuario/datos';
         // http://localhost:8080/myapp/usuario/login
         var datos = {
             "email": email
-        }
+        };
 
         $.ajax({
             url: url,
             type: "POST",
-            data: datos
-        }).done(function( response ) {
+            //data: datos,
+            data: JSON.stringify(email),
+            contentType: "application/json",
+            statusCode: {
 
-            console.log(response);
+                201: function (result) {
+                    swal({
+                        title: "¡Revisa tu bandeja de entrada!",
+                        text: "¡Ya le hemos enviado su contraseña con el correo con el que se registro!",
+                        type: "success",
+                        confirmButtonText: "Aceptar"
+                    });
 
-            if(response.status === 418) {
-                swal({
-                    title: "¡Vaya!",
-                    text: "¡Parece que no se ha encontrado el correo!",
-                    type: "error",
-                    confirmButtonText: "Pruebo de nuevo"
-                });
+                    // redirigimos a otra página
+                    window.location.href = 'index.html';
+                },
+
+                418: function () {
+
+                    swal({
+                        title: "¡Vaya!",
+                        text: "¡Parece que no se ha encontrado el correo!",
+                        type: "error",
+                        confirmButtonText: "Pruebo de nuevo"
+                    });
+                }
             }
-            if(response.status === 201){
-                swal({
-                    title: "¡Revisa tu bandeja de entrada!",
-                    text: "¡Ya le hemos enviado su contraseña con el correo con el que se registro!",
-                    type: "success",
-                    confirmButtonText: "Aceptar"
-                });
 
-                // redirigimos a otra página
-                window.location.href = 'index.html'
-            }
         });
 
     });
 
-
 });
-
