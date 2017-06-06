7a558df8f676c43bb3e812d3a5e323229f6e4c1d
Roberto <roberto.arranz.93@gmail.com>
2017-06-06 17:55:12 +0200

    REdiseño de la Eetakedex

--- public/web/scripts/eetakedexScript.js
+++ public/web/scripts/eetakedexScript.js
@@ -6,13 +6,31 @@ $(document).ready(function() {
         for (var i = 0; i < json.length; i++) {
             actual_id = json[i].id;
             actual_eetakemon = json[i].nombre;
-            $("#eetakedex_generated").append("<div class='col-md-2' id='"+actual_id+"'>");
-            document.getElementById(actual_id).innerHTML = "<div class='thumbnail' id='"+actual_eetakemon+"' style='align-content: center'>" ;
-            document.getElementById(actual_eetakemon).innerHTML = "<img src='"+json[i].imagen+"' alt='"+actual_eetakemon+"' style='width:100%'><div class='caption'><p>"+actual_eetakemon+"</p>";
+            $("#eetakedex_generated").append("<div class='col-md-4' id='"+actual_id+"'>");
+            document.getElementById(actual_id).innerHTML = "<div class='thumbnail' id='"+actual_eetakemon+"' style='align-content: center;border-radius: 10px'>" ;
+            document.getElementById(actual_eetakemon).innerHTML = "<img src='"+json[i].imagen+"' alt='"+actual_eetakemon+"' style='width:100%'>" +
+                "<div class='well well-sm' style='border-radius: 10px; height: 200px'><h4><strong>"+actual_eetakemon+"</strong></h4>" +
+                "<h4>Habilidad: "+json[i].habilidad+"</h4>" +
+                "<p>"+json[i].descripcion+"</p>";
         }
 
     })
 })
 
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Chrome, Safari and Opera
+    document.documentElement.scrollTop = 0; // For IE and Firefox
+}
 
 
