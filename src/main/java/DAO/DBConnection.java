ed0528375f57b5e1010311388ebe8d3d4bc9fd3b
munozloisivan <munozloisivan@gmail.com>
2017-06-17 16:13:27 +0200

    pequeño arreglo guapis

--- src/main/java/DAO/DBConnection.java
+++ src/main/java/DAO/DBConnection.java
@@ -24,14 +24,14 @@ public class DBConnection {
             String host = "localhost", database = "juego";
             int port = 3306;
             Class.forName("com.mysql.cj.jdbc.Driver");
-            //String url = "jdbc:mysql://" + host + ":" + port + "/" + database;
-          //  Properties info = new Properties();
-          //  info.setProperty("user", "ea0");
-            //info.setProperty("password", "mysql");
-           // info.setProperty("useSSL", "false");
-           // info.setProperty("serverTimezone", "UTC");
-           // con = DriverManager.getConnection(url, info);
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/juego", "ea0","");
+            String url = "jdbc:mysql://" + host + ":" + port + "/" + database;
+           Properties info = new Properties();
+           info.setProperty("user", "ea0");
+           //info.setProperty("password", "mysql");
+           info.setProperty("useSSL", "false");
+           info.setProperty("serverTimezone", "UTC");
+            con = DriverManager.getConnection(url, info);
+         //   con = DriverManager.getConnection("jdbc:mysql://localhost:3306/juego", "ea0",null);
             logger.info("Conexión BBDD creada \n");
         }
         catch (Exception e){
