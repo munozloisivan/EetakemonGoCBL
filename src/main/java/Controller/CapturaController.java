10f56187d1052ae0e91bdb654a928c3de2114320
munozloisivan <munozloisivan@gmail.com>
2017-06-19 00:51:26 +0200

    arreglado recuperardatos

--- src/main/java/Controller/CapturaController.java
+++ src/main/java/Controller/CapturaController.java
@@ -3,9 +3,7 @@ package Controller;
 import Modelo.Captura;
 
 import javax.inject.Singleton;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -42,6 +40,15 @@ public class CapturaController {
             String noResult = "No tiene capturas.";
             return Response.status(404).entity(noResult).build();
         }
+    }
 
+    @POST
+    @Path("/{iduser}/capture")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response setCapturaToUsuario(@PathParam("iduser") int iduser, Captura captura)
+    {
+        Captura nuevaCaptura = new Captura();
+        nuevaCaptura.insertarCaptura(captura,iduser);
+        return Response.status(201).entity(nuevaCaptura).build();
     }
 }
